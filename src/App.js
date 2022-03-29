756ea07d4feb0c6b7803446ac87926c2d40436fa
unknown <binyaalex@gmail.com>
2022-03-29 09:56:52 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FitnessSquare from './components/FitnessSquare'
+
+function App() {
+	return (
+		<>
+			<FitnessSquare />
+		</>
+	)
+}
+
+export default App;
---
+++ src/components/FitnessSquare.js
@@ -0,0 +1,9 @@
+const FitnessSquare = ({}) => {
+	return (
+		<main className='fitnessSquare' style={{backgroundImage: 'url(https://www.onebody.co.il/wp-content/uploads/2020/07/yoga_1.jpg)'}} >
+			<h1 className='fitnessSquareHeader' >yoga</h1>
+		</main>
+	)
+}
+
+export default FitnessSquare
