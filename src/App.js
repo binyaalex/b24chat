0feeb9d49608e9a11d646fe897397abc472ffdd5
unknown <binyaalex@gmail.com>
2022-03-29 12:12:43 +0300

    make footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Head from './components/Head'
 import FitnessSquareContainer from './components/FitnessSquareContainer'
+import Footer from './components/Footer'
 
 function App() {
 	return (
-		<>
-			<Head text='fitness' />
-			<FitnessSquareContainer img='url(https://www.onebody.co.il/wp-content/uploads/2020/07/yoga_1.jpg)' text='yoga' />
-		</>
+		<div className='fitnessPageDad' >
+			<div className='fitnessPage' >
+				<Head text='fitness' />
+				<FitnessSquareContainer img='url(https://www.onebody.co.il/wp-content/uploads/2020/07/yoga_1.jpg)' text='yoga' />
+				<Footer />
+			</div>
+		</div>
 	)
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+	return (
+		<footer className='footer' >
+			<div className='sos' >SOS</div>
+			<i class="fas fa-regular fa-house-user"></i>
+			<i class="fas fa-solid fa-book"></i>
+			<i class="fas fa-regular fa-bell"></i>
+			<i class="fas fa-solid fa-user"></i>
+			<i class="fas fa-regular fa-comments comments"></i>
+		</footer>
+	)
+}
+
+export default Footer
