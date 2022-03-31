432047ec394ff438ddbdceb28feb6116e094929f
unknown <binyaalex@gmail.com>
2022-03-31 11:23:32 +0300

    make bold part in notification massage color

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -4,17 +4,23 @@ const Notifications = () => {
 	const containerArr = [
 		{
 			img: 'https://cdn5.vectorstock.com/i/1000x1000/77/54/doctor-vector-677754.jpg',
-			massage: 'Congratulations! your baby is 6 weeks old today!',
+			massageBeforeBold: 'Congratulations! your baby is ',
+			bold: '6 weeks',
+			massageAfterBold: ' old today!',
 			date: 'Today, 11:00 am',
 		},
 		{
 			img: 'https://cdn5.vectorstock.com/i/1000x1000/77/54/doctor-vector-677754.jpg',
-			massage: 'You have booked an online consultation with a doctor Hanna Smith',
+			massageBeforeBold: 'You have booked an online consultation with a doctor ',
+			bold: 'Hanna Smith',
+			massageAfterBold: '',			
 			date: '15 Mar 2022, 12:30 am',
 		},
 		{
 			img: 'https://cdn5.vectorstock.com/i/1000x1000/77/54/doctor-vector-677754.jpg',
-			massage: 'Time to take your medicine! B12 Vitamin',
+			massageBeforeBold: 'Time to take your medicine! ',
+			bold: 'B12 Vitamin',
+			massageAfterBold: '',			
 			date: '14 Mar 2022, 08:10 am',
 		},
 	]
@@ -23,8 +29,15 @@ const Notifications = () => {
 		<main className='notifications' >
 			{
 				containerArr.map((el, i) => {
+					console.log(el.massageAfterBold)
 					return (
-						<NotificationsSquare img={el.img} massage={el.massage} date={el.date} /> 
+						<NotificationsSquare
+							img={el.img}
+							massageBeforeBold={el.massageBeforeBold}
+							bold={el.bold}
+							massageAfterBold={el.massageAfterBold}
+							date={el.date}
+						/> 
 					)
 				})
 			}
--- src/components/NotificationsSquare.js
+++ src/components/NotificationsSquare.js
@@ -1,11 +1,12 @@
-const NotificationsSquare = ({img, massage, date}) => {
+const NotificationsSquare = ({img, massageBeforeBold, bold, massageAfterBold, date}) => {
+	console.log(massageAfterBold)
 	return (
 		<section className='NotificationsSquare' >
 			<div className='NotificationsImgDiv' >
 				<img className='NotificationsImg' src={img} />
 			</div>
 			<div className='NotificationsTextDiv' >
-				<h1 className='NotificationsTextMassage' >{massage}</h1>
+				<h1 className='NotificationsTextMassage' >{massageBeforeBold} <span className='NotificationsTextMassageSpan'>{bold}</span> {massageAfterBold}</h1>
 				<date className='NotificationsTextDate' >{date}</date>
 			</div>
 			<button className='NotificationsBtn' >ok</button>
