810ce2273306d3fcbc6c48c92432a7e93bc7b4c1
unknown <binyaalex@gmail.com>
2022-03-31 10:51:06 +0300

    made Notifications page finish design main things

--- src/components/NotificationsSquare.js
+++ src/components/NotificationsSquare.js
@@ -1,9 +1,16 @@
-const FitnessSquare = ({img, text}) => {
+const NotificationsSquare = ({img, text}) => {
 	return (
-		<section className='fitnessSquare' style={{backgroundImage: img}} >
-			<h1 className='fitnessSquareHeader' >{text}</h1>
+		<section className='NotificationsSquare' >
+			<div className='NotificationsImgDiv' >
+				<img className='NotificationsImg' src='https://cdn5.vectorstock.com/i/1000x1000/77/54/doctor-vector-677754.jpg' />
+			</div>
+			<div className='NotificationsTextDiv' >
+				<h1 className='NotificationsTextHeader' >Congratulations! your baby is 6 weeks old today!</h1>
+				<date className='NotificationsTextDate' >Today, 11:00 am</date>
+			</div>
+			<button className='NotificationsBtn' >ok</button>
 		</section>
 	)
 }
 
-export default FitnessSquare
+export default NotificationsSquare
