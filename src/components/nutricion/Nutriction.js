8f82bdf1baa0ae3df9ffb78aafe60768ab4c3365
unknown <binyaalex@gmail.com>
2022-04-04 12:35:25 +0300

    start making nutricion page

---
+++ src/components/nutricion/Nutriction.js
@@ -0,0 +1,18 @@
+const Nutricion = () => {
+	return (
+		<main className='nutricion' >
+			<section className='nutricionMenu' >
+				<div className='nutricionMenuMain' >
+					<div className='nutricionMenuMainHead' >
+						<h1 className='nutricionMenuMainHeadHeader' >today</h1>
+						<button className='nutricionMenuMainHeadBtn' >></button>
+					</div>
+				</div>
+			</section>
+			<section>
+			</section>
+		</main>
+	)
+}
+
+export default Nutricion
