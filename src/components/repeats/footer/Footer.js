9176c52fdd314ede218b3f34fe435163d6e5fa33
unknown <binyaalex@gmail.com>
2022-04-05 18:29:55 +0300

    finish footer by xd very accurate

--- src/components/repeats/footer/Footer.js
+++ src/components/repeats/footer/Footer.js
@@ -3,13 +3,15 @@ import './Footer.css';
 const Footer = () => {
 	return (
 		<footer className='footer' >
-			<img className='sos' src="/images/footer/sos.png" />
-			<img className='grayIcon home' src="/images/footer/home.png" />
-			<img className='grayIcon book' src="/images/footer/book.png" />
-			<img className='grayIcon grayBell' src="/images/footer/grayBell.png" />
-			<img className='grayIcon user' src="/images/footer/user.png" />
-			{<i class="fas fa-regular fa-comments comments"></i>}
-			{/*<img className='chat' src="/images/footer/chat.png" />*/}
+			<div className='sosAndGray'>
+				<img className='sos' src="/images/footer/sos.png" />
+				<img className='grayIcon home' src="/images/footer/home.png" />
+				<img className='grayIcon book' src="/images/footer/book.png" />
+				<img className='grayIcon grayBell' src="/images/footer/grayBell.png" />
+				<img className='grayIcon user' src="/images/footer/user.png" />
+				{/*{<i class="fas fa-regular fa-comments comments"></i>}*/}
+			</div>
+			{<img className='chat' src="/images/footer/chat.png" />}
 		</footer>
 	)
 }
