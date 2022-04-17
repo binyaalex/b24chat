2f52ec4e8f73574b3a26bd09e2b8e79adb317c61
unknown <binyaalex@gmail.com>
2022-04-17 17:58:51 +0300

    fix the problem on the menu button work only after second press

--- src/components/repeats/head/Head.js
+++ src/components/repeats/head/Head.js
@@ -28,10 +28,10 @@ const Head = ({text}) => {
 
 	const displayMenu = () => {
 		const menu = document.querySelector('.menu')
-		if (menu.style.display === 'none') {
-			menu.style.display = 'block';
-		} else {
+		if (menu.style.display === 'block') {
 			menu.style.display = 'none';
+		} else {
+			menu.style.display = 'block';
 		}
 	}
 
