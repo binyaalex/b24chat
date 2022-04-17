1d067662d785f3f8ce16ed7549ca7798004d1e35
unknown <binyaalex@gmail.com>
2022-04-17 12:23:35 +0300

    finish make start page

--- src/components/headPages/Start.js
+++ src/components/headPages/Start.js
@@ -1,3 +1,4 @@
+import './Start.css';
 import React, { useEffect } from 'react';
 
 const Start = ({setPageInfo}) => {
@@ -8,27 +9,27 @@ const Start = ({setPageInfo}) => {
 
 	return (
 		<div className='startPage' >
-			<img className='startImg' src='' />
-			<h1 className='startH1' > </h1>
-			<h2 className='startH2' > </h2>
+			<img className='startImg' src='/images/start/congrats.png' />
+			<h1 className='startH1' >congreatulations</h1>
+			<h2 className='startH2' >Choose a start date</h2>
 			<div className='startDates' >
-				<div className='startDateUp' >
-					<div className='day' > </div>
-					<div className='month' > </div>
-					<div className='year' > </div>
+				<div className='date startDateUp' >
+					<div className='day' >27</div>
+					<div className='month' >feb</div>
+					<div className='year' >2021</div>
 				</div>
-				<div className='startDateChoosen' >
-					<div className='day' > </div>
-					<div className='month' > </div>
-					<div className='year' > </div>
+				<div className='date startDateChoosen' >
+					<div className='day' >28</div>
+					<div className='month' >feb</div>
+					<div className='year' >2022</div>
 				</div>
-				<div className='startDateDown' >
-					<div className='day' > </div>
-					<div className='month' > </div>
-					<div className='year' > </div>
+				<div className='date startDateDown' >
+					<div className='day' >01</div>
+					<div className='month' >mar</div>
+					<div className='year' ></div>
 				</div>
 			</div>
-			<div className='startSubmit' > </div>
+			<button className='startSubmit' >submit</button>
 		</div>
 	)
 }
