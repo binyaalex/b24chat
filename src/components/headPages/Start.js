c02e88ef117b3dceceaf2a60e6b65a1b7a618600
unknown <binyaalex@gmail.com>
2022-04-18 09:14:33 +0300

    change start page to hebrew

--- src/components/headPages/Start.js
+++ src/components/headPages/Start.js
@@ -6,32 +6,32 @@ import Dates from './Dates';
 const Start = ({setPageInfo}) => {
 
 	useEffect(() => {
-		setPageInfo({header: 'Start', className: 'StartPage'})
+		setPageInfo({header: 'התחלה', className: 'StartPage'})
 	},[]);
 
 	return (
 		<div className='startPage' >
 			<img className='startImg' src='/images/start/congrats.png' />
-			<h1 className='startH1' >congreatulations</h1>
-			<h2 className='startH2' >Choose a start date</h2>
+			<h1 className='startH1' >מזל טוב!! בשעה טובה</h1>
+			<h2 className='startH2' >בחרי תאריך</h2>
 			<div className='startDates' >
 				<div className='date startDateUp' >
 					<div className='day' >27</div>
-					<div className='month' >feb</div>
+					<div className='month' >ינואר</div>
 					<div className='year' >2021</div>
 				</div>
 				<div className='date startDateChoosen' >
 					<div className='day' >28</div>
-					<div className='month' >feb</div>
+					<div className='month' >פברואר</div>
 					<div className='year' >2022</div>
 				</div>
 				<div className='date startDateDown' >
 					<div className='day' >01</div>
-					<div className='month' >mar</div>
+					<div className='month' >מרץ</div>
 					<div className='year' ></div>
 				</div>
 			</div>
-			<button className='startSubmit' >submit</button>
+			<button className='startSubmit' >שלחי</button>
 			<Dates />
 		</div>
 	)
